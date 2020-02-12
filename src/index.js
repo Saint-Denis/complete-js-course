70b47d7994ff2167c7405b77844369b0c4cac1c5
Денис <saint_denis@MacBook-Pro-Denis.local>
2020-02-12 20:07:10 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,78 +1,89 @@
 const isAllTrue = (array, fn) => {
   let result
-    if (!Array.isArray(array) || !array.length) {
-      throw new Error('empty array')
-    } else if (typeof fn !== 'function') {
-      throw new Error('fn is not a function')
-    }
-    for (let i = 0; i < array.length; i++) {
-      result = fn(array[i])
-    }
-  
+
+  if (!Array.isArray(array) || !array.length) {
+    throw new Error('empty array')
+  } else if (typeof fn !== 'function') {
+    throw new Error('fn is not a function')
+  }
+
+  for (let i = 0; i < array.length; i++) {
+    result = fn(array[i])
+  }
+
   return result
 }
 
 
 const isSomeTrue = (array, fn) => {
-    if (!Array.isArray(array) || !array.length) {
-      throw new Error('empty array')
-    } else if (typeof fn !== 'function') {
-      throw new Error('fn is not a function')
-    }
-    for (let i = 0; i < array.length; i++) {
-      if (fn(array[i]) === true) return true
-    }
+  if (!Array.isArray(array) || !array.length) {
+    throw new Error('empty array')
+  } else if (typeof fn !== 'function') {
+    throw new Error('fn is not a function')
+  }
+
+  for (let i = 0; i < array.length; i++) {
+    if (fn(array[i]) === true) return true
+  }
+  
   return false
 }
 
-/*
- Задание 3:
-
- 3.1: Функция принимает заранее неизветсное количество аргументов, первым из которых является функция fn
- Функция должна поочередно запустить fn для каждого переданного аргумента (кроме самой fn)
-
- 3.2: Функция должна вернуть массив аргументов, для которых fn выбросила исключение
-
- 3.3: Необходимо выбрасывать исключение в случаях:
-   - fn не является функцией (с текстом "fn is not a function")
- */
-function returnBadArguments(fn, ...rest) {
-  if (rest === undefined) return []
-  
-  let arrayErrors = []
+const returnBadArguments = (fn, ...rest) => {
   if (typeof fn !== 'function') {
     throw new Error('fn is not a function')
   }
 
-  [...rest].forEach((el, i, array) => {
+  if (!rest.length) return []
+
+  let arrayErrors = []
+
+  rest.forEach((el, i, array) => {
     try {
       fn(el, i, array)
     } catch (e) {
       arrayErrors.push(array[i])
     }
-    console.log('arrayErrors', arrayErrors)
-    return arrayErrors
   })
+  
+  return arrayErrors
 }
 
-/*
- Задание 4:
-
- 4.1: Функция имеет параметр number (по умолчанию - 0)
-
- 4.2: Функция должна вернуть объект, у которого должно быть несколько методов:
-   - sum - складывает number с переданными аргументами
-   - dif - вычитает из number переданные аргументы
-   - div - делит number на первый аргумент. Результат делится на следующий аргумент (если передан) и так далее
-   - mul - умножает number на первый аргумент. Результат умножается на следующий аргумент (если передан) и так далее
-
- Количество передаваемых в методы аргументов заранее неизвестно
-
- 4.3: Необходимо выбрасывать исключение в случаях:
-   - number не является числом (с текстом "number is not a number")
-   - какой-либо из аргументов div является нулем (с текстом "division by 0")
- */
-function calculator() {
+const calculator = (number = 0) => {
+  if (!isFinite(number)) {
+    throw new Error("number is not a number")
+  }
+  return {
+    sum (...args) {
+      const result = [...args].reduce((acc, current) => {
+        return acc + current
+      }, number)
+    return result
+    },
+    dif (...args) {
+      const result = [...args].reduce((acc, current) => {
+        return acc - current
+    }, number)
+    return result
+    },
+    div (...args) {
+      [...args].forEach(number => {
+        if (number === 0) {
+        throw new Error("division by 0")
+      }
+    })
+    const result = [...args].reduce((acc, current) => {
+      return acc / current
+    }, number)
+    return result
+    },
+    mul (...args) {
+      const result = [...args].reduce((acc, current) => {
+        return acc * current
+    }, number)
+    return result
+    }
+  }
 }
 
 /* При решении задач, пострайтесь использовать отладчик */
